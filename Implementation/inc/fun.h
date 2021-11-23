826874fd5521cbbd0f2f7df40dda7dd719024d91
Nirmalrg2898 <94277405+Nirmalrg2898@users.noreply.github.com>
2021-11-23 23:06:01 +0530

    Update fun.h

--- Implementation/inc/fun.h
+++ Implementation/inc/fun.h
@@ -1,10 +1,8 @@
 #ifndef _FUN_H
 #define _FUN_H
 void wel_come(void);
-void title(void);
 void login();
 void menu(void);
-void title(void);
 void deleteproduct(void);
 void add_item();
 void read_item();
